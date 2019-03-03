89c82fcd94c0bbe90a22c6790c93e54ca6845ab6
Andrii Petruk <andrey.petruk@gmail.com>
2019-03-03 19:47:12 +0200

    Update sites.go

--- luminate/sites.go
+++ luminate/sites.go
@@ -1,9 +1,9 @@
-// Copyright (c) 2019, Andrii Petruk
+// Copyright (c) 2019, Andrii Petruk. All rights reserved.
 //
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package luminate
+package goluminate
 
 
 import (
